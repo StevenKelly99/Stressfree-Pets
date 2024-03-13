62db39be78a8384d64fb5d71fe18b4c17ff67b59
HabibaNour <b00151078@mytudublin.ie>
2024-03-13 13:02:30 +0000

    new

---
+++ Website/public/PetWalking.php
@@ -0,0 +1,19 @@
+<?php require "../layout/header.php"?>
+
+<body>
+<div class="petWalking">
+    <img src="../images/petWalking.jpg" height="" width="" alt="petWalkingImg">
+    <p>Pet Walking service is providing help for your pets such as taking your dog to the vet,
+        helping you dog lose weight, keep tracking of your dog's food,
+        keep an eye on your pet's health, helping your dog to learn new thing,
+        clean you dog's stuff, give your dog a bath</p>
+
+    <p>if you think this service is perfect for you can
+        <button type="button">Book here</button></p>
+</div>
+
+</body>
+
+
+
+
