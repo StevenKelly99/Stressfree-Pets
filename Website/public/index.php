8b067c2b7c7622ea645eee0bb9524aacb882213f
HabibaNour <b00151078@mytudublin.ie>
2024-03-13 12:45:27 +0000

    new

--- Website/public/index.php
+++ Website/public/index.php
@@ -19,7 +19,38 @@
         </div>
     </div>
 
-    <div class="container">
+
+
+<?php require "css/style.css";?>
+
+<body>
+
+<div class="grids">
+    <div class="daycare">
+        <img src="img/daycare.jpg" height="250" width="350" alt="picture">
+        <h3>Daycare Service</h3>
+        <p>This service provides many advantages for your pet such as improving socialization with other dogs and avoid anxiety, and it is a good option for people who are working.
+            If you wish to find more information about the service, you can <a href="daycare.php"><strong>click here</strong></a> </p>
+    </div>
+    <div class="petWalking">
+        <img src="img/petWalking.jpg" height="250" width="350" alt="picture">
+        <h3>Pet Walking Service</h3>
+        <p>This service keeps us focused on your pet's health, trains your pet, and visits the vet if needed. If you need to find more
+            information, you can <a href="petWalking.php.php"><strong>click here</strong></a></p>
+    </div>
+
+    <div class="petSitting">
+        <img src="img/petSitting.jpg" height="250" width="350" alt="picture">
+        <h3>Pet Sitting Service</h3>
+        <p>Pet sitting services provide a pet sitter that visits our home and spends the time you decide with your pet.
+            If you need to know more about the service, you can <a href="petSitting.php"><strong>click here</strong></a></p>
+    </div>
+</div>
+</body>
+
+
+
+<div class="container">
         <div class="row">
             <?php foreach ($pets as $cutePet) { ?>
                 <div class="col-lg-4 pet-list-item">
