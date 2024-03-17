d2a115a376d809bafcc45c80451310ea0a9a3911
Rochelle Mullen <b00156311@mytudublin.ie>
2024-03-17 12:46:45 +0000

    Update Clean.php

--- Website/src/Clean.php
+++ Website/src/Clean.php
@@ -5,9 +5,14 @@ namespace src;
 class Clean
 {
 function clean_input($data){
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+    if($data != null){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data,ENT_QUOTES|ENT_SUBSTITUTE,"UTF-8");
+        return $data;
+    }
+    else
+        return null;
+
 }
 }
