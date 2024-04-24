3e478b750c069cc18e5707a6a9b5d09ac358181c
HabibaNour <b00151078@mytudublin.ie>
2024-04-24 02:54:03 +0100

    semaphore

--- Website/database/customer.sql
+++ Website/database/customer.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE customers;
-use customers;
-CREATE TABLE CustomerApplecation (
-     id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-     firstname VARCHAR(30) NOT NULL,
-     lastname VARCHAR(30) NOT NULL,
-     Address VARCHAR(50) NOT NULL,
-     email VARCHAR(50) NOT NULL,
-     phone INT(20) NOT NULL,
-     dogName VARCHAR(50) NOT NULL,
-     dogType VARCHAR(50) NOT NULL,
-     age INT(3),
-     addinfo VARCHAR(50) NOT NULL
+CREATE DATABASE SETcustomer;
+use SETcustomer;
+CREATE TABLE SETCustomerApplication (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    Address VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone INT(20) NOT NULL,
+    dogName VARCHAR(50) NOT NULL,
+    dogType VARCHAR(50) NOT NULL,
+    age INT(3),
+    addinfo VARCHAR(50) NOT NULL
 );
